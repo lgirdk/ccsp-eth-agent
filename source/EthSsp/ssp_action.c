f38fec25de00e7b1e5b3c957def1ea626d71f42d
kkumar564 <kishore_kumar2@comcast.com>
2019-11-11 11:42:52 +0530

    TRDKB-367,RDKB-26266 : Integrate support for XML to C in CcspEthAgent
    Reason for change: Size reduction
    Test Procedure: Ensure tr181 xml doesn't exist.
    Risks: None
    Signed-off-by: kishore_kumar2@comcast.com
    Change-Id: I487d7b1678a015061c8d5379b703ef899dfcd846

--- source/EthSsp/ssp_action.c
+++ source/EthSsp/ssp_action.c
@@ -54,6 +54,7 @@
 #include "plugin_main.h"
 #include "dslh_dmagnt_interface.h"
 #include "ccsp_trace.h"
+#include "dm_pack_create_func.h"
 
 PDSLH_CPE_CONTROLLER_OBJECT     pDslhCpeController        = NULL;
 PCOMPONENT_COMMON_ETHAGENT          g_pComponent_COMMON_ethagent  = NULL;
@@ -180,11 +181,11 @@ ssp_engage
     }
 
     returnStatus =
-        pDslhCpeController->RegisterCcspDataModel
+        pDslhCpeController->RegisterCcspDataModel2
             (
                 (ANSC_HANDLE)pDslhCpeController,
                 CrName, /* CCSP_DBUS_INTERFACE_CR,*/              /* CCSP CR ID */
-                CCSP_DATAMODEL_XML_FILE,             /* Data Model XML file. Can be empty if only base data model supported. */
+                DMPackCreateDataModelXML,             /* Data Model XML file. Can be empty if only base data model supported. */
                 CCSP_COMPONENT_NAME_ETHAGENT,            /* Component Name    */
                 CCSP_COMPONENT_VERSION_ETHAGENT,         /* Component Version */
                 CCSP_COMPONENT_PATH_ETHAGENT,            /* Component Path    */
