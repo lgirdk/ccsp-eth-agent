f55f4da70ec43fa23b8262ba05498cd33ebd6339
mkrish802 <Manoj_Krishnan@comcast.com>
2020-08-25 12:45:16 +0000

    RDKB-31425 [Security] Drop process privileges on WebPA - CcspEthAgent
    Reason for change: Drop root privileges for WebPA process
    
    Test Procedure:
    Verify Get and Set of "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.NonRootSupport.Enable"
    Check whether WebPA runs in nonroot mode when RFC is enabled
    Risks: Medium
    
    Signed-off-by: mkrish802 <Manoj_Krishnan@comcast.com>
    (cherry picked from commit 438b9af912d677d3839024cea054ce48861f6a81)
    (cherry picked from commit a4a325613d9fb5db8dd8b1054b346da24e1b4248)
    (cherry picked from commit ba04f262160decbdb0035636094be2f0a5f2bd66)
    Change-Id: Ic0e142aa6459661e817b216e15c50b5f44123e23

--- source/EthSsp/ssp_main.c
+++ source/EthSsp/ssp_main.c
@@ -53,8 +53,6 @@
 
 cap_user appcaps;
 
-#define ARRAY_SIZE(x)  (sizeof(x) / sizeof(x[0]))
-
 extern char*                                pComponentName;
 char                                        g_Subsystem[32]         = {0};
 extern ANSC_HANDLE bus_handle;
@@ -238,11 +236,8 @@ int main(int argc, char* argv[])
     BOOL                            bRunAsDaemon       = TRUE;
     int                             cmdChar            = 0;
     int                             idx = 0;
-    const cap_value_t cap_add[] = {CAP_KILL};
-    const cap_value_t cap_drop[] = {};
     appcaps.caps = NULL;
-    appcaps.add_count = ARRAY_SIZE(cap_add);
-    appcaps.drop_count = ARRAY_SIZE(cap_drop);
+    appcaps.user_name = NULL;
     char buf[8] = {'\0'};
     extern ANSC_HANDLE bus_handle;
     char *subSys            = NULL;  
@@ -257,7 +252,6 @@ int main(int argc, char* argv[])
     if( buf != NULL )  {
         if (strncmp(buf, "true", strlen("true")) == 0) {
             init_capability();
-            prepare_caps(&appcaps,cap_add,cap_drop);
             drop_root_caps(&appcaps);
             update_process_caps(&appcaps);
             read_capability(&appcaps);
@@ -375,7 +369,6 @@ CcspTraceWarning(("\nAfter Cdm_Init\n"));
     if( buf != NULL )  {
         if (strncmp(buf, "true", strlen("true")) == 0) {
             init_capability();
-            prepare_caps(&appcaps,cap_add,cap_drop);
             drop_root_caps(&appcaps);
             update_process_caps(&appcaps);
             read_capability(&appcaps);
