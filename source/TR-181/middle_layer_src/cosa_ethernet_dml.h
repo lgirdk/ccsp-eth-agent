f46e93a713ed4da566c1731f44023f37bf87d231
Sasidharan, Sandeep (Business Partner) <ssa49@skyglobal.mail.onmicrosoft.com>
2021-10-20 06:12:52 +0000

    Revert "Revert "RDKB-37966 Modify Eth Agent to perform HW Reconfiguration""
    This reverts commit e12feb968c46bd96966aed5ef0d7afb92e89f4f7.
    
    (cherry picked from commit b4c51d82858142f348201fa5118662a9c98409e0)
    Change-Id: Ib65e7b9694e0620fae2f73f5d590790dc48345ab

--- source/TR-181/middle_layer_src/cosa_ethernet_dml.h
+++ source/TR-181/middle_layer_src/cosa_ethernet_dml.h
@@ -19,13 +19,13 @@
 
 /**********************************************************************
    Copyright [2014] [Cisco Systems, Inc.]
- 
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
- 
+
        http://www.apache.org/licenses/LICENSE-2.0
- 
+
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -417,18 +417,18 @@ EthRdkInterface_SetParamStringValue
         char*                       ParamName,
         char*                       pString
     );
-ANSC_HANDLE 
-EthRdkInterface_AddEntry 
-    ( 
+ANSC_HANDLE
+EthRdkInterface_AddEntry
+    (
         ANSC_HANDLE hInsContext,
-        ULONG* pInsNumber 
+        ULONG* pInsNumber
     );
 
-ULONG 
+ULONG
 EthRdkInterface_DelEntry
-    ( 
+    (
         ANSC_HANDLE hInsContext,
-        ANSC_HANDLE hInstance 
+        ANSC_HANDLE hInstance
     );
 
 BOOL
@@ -466,5 +466,5 @@ EthRdkInterface_SetParamUlongValue
     char *ParamName,
     ULONG uValue
     );
-#endif  // FEATURE_RDKB_WAN_MANAGER    
+#endif  // FEATURE_RDKB_WAN_MANAGER
 #endif
