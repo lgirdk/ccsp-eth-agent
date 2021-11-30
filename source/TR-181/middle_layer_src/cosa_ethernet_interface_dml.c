30c338495a8fa4d92758eb2c3738162fb61d957a
Sandeep Sasidharan <sandeep.sasidharan@sky.uk>
2021-11-30 05:45:19 +0000

    RDKB-37966 Modify Eth Agent to perform HW Reconfiguration
    Reason for change: Added new datamodel.
    
    Test Procedure:
    1. Build wont break.
    2. Fixing merge issue.
    Risks: Low
    
    Signed-off-by: Sandeep Sasidharan <sandeep.sasidharan@sky.uk>
    Change-Id: Ib6c6f083e7909baec5a81b1fb1f0746ee5cc5ce4

--- source/TR-181/middle_layer_src/cosa_ethernet_interface_dml.c
+++ source/TR-181/middle_layer_src/cosa_ethernet_interface_dml.c
@@ -600,14 +600,6 @@ Interface_SetParamBoolValue
         return TRUE;
     }
 
-#ifdef FEATURE_RDKB_WAN_UPSTREAM
-    if( AnscEqualString(ParamName, "Upstream", TRUE))
-    {
-        EthInterfaceSetUpstream( pEthernetPortFull->Cfg.InstanceNumber - 1, bValue );
-        return TRUE;
-    }
-#endif  // FEATURE_RDKB_WAN_UPSTREAM
-
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
 }
