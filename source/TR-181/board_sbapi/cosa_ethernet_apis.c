55f9e646ce4dd962353c122a51bdd03331a2509c
Devin Mungan <dmungan@libertyglobal.com>
2023-11-30 13:25:45 +0100

    remove EthAgent dependency to WanManager at init phase ( OFW-5361 )
    [Issue]
    ccsp-eth-agent tries to set PhyStatus and PhyPath of WAN interface
    to WanManager at init phase but this is no longer needed with
    WanManager Phase4 changes.
    
    In phase4,
    PhyStatus is set from link event handler thread.
    PhyPath is already set as a preconfigured psm value.
    
    [Solution]
    disable PhyPath and PhyStatus setting  at init phase of ccsp-eth-agent.
    
    [Testing Performed]
    NA
    
    [Upstream Candidate]
    Yes
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- source/TR-181/board_sbapi/cosa_ethernet_apis.c
+++ source/TR-181/board_sbapi/cosa_ethernet_apis.c
@@ -2781,7 +2781,7 @@ CosaDmlEthInit(
             CcspTraceError(("CcspHalExtSw_setEthWanEnable failed during bootup\n"));
         }
     }
-
+#ifndef WAN_MANAGER_UNIFICATION_ENABLED
     if(CosaDmlMapWanCPEtoEthInterfaces(WanOEInterface, sizeof(WanOEInterface)) == ANSC_STATUS_SUCCESS) {
 #if defined (INTEL_PUMA7) && !defined (AUTOWAN_ENABLE)
         if(FALSE) 
@@ -2810,6 +2810,7 @@ CosaDmlEthInit(
             }
         }
     }
+#endif //WAN_MANAGER_UNIFICATION_ENABLED
 #endif
 
 #else
